6963f0b39ab330a02090dcd9b56ff0f22a627456
Yannick Heinrich <yannick.heinrich@gmail.com>
2020-09-17 09:25:00 +0200

    Fix async

--- Sources/StrasbourgParkAPI/Combine.swift
+++ Sources/StrasbourgParkAPI/Combine.swift
@@ -9,7 +9,7 @@ import Foundation
 #if canImport(Combine)
 import Combine
 
-@available(iOS 13.0, macOS 10.15, *)
+@available(iOS 13.0, macOS 10.15, watchOS 6.0, *)
 extension ParkingAPIClient {
 
     /// FRP version of `getLocation`
@@ -17,7 +17,7 @@ extension ParkingAPIClient {
     public func getLocation() -> Future<[StrasbourgParkAPI.LocationOpenData], Error> {
         return Future { [unowned self] result in
             self.getLocations { (r) in
-                result(r)
+                return result(r)
             }
         }
     }
@@ -27,7 +27,7 @@ extension ParkingAPIClient {
     public func getStatus() -> Future<[StrasbourgParkAPI.StatusOpenData], Error> {
         return Future { [unowned self] o in
             self.getStatus { (result) in
-                o(result)
+                return o(result)
             }
         }
     }
